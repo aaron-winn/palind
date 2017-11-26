808c38c1c64b99057e618910ac90c74ff68b1268
John Doe <johndoe@example.com>
2017-11-26 11:58:36 -0700

    maybe

---
+++ palidrone.rb
@@ -0,0 +1,28 @@
+ #get word
+ #save word
+ #
+ puts("What word do you want to check")
+ input = gets.strip
+
+ def swap
+    input.clone = second
+second
+    input.split("")
+ word [input]
+ def reverse(string)
+    i = 0
+    abc = [] # creating empty array
+    while i < string.length
+      abc.unshift(string[i]) #populating empty array in reverse
+      i = i + 1
+  end
+  return abc.join   
+  end
+  puts ("In reverse:  " + reverse(string))  
+  
+
+
+ end
+ 
+ 
+
